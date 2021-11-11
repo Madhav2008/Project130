8022c0ca33c4454d699ff0b09b065c3b38a958c0
Madhav Arora <72864817+Madhav2008@users.noreply.github.com>
2021-11-11 12:44:16 +0530

    Update code.py

--- code.py
+++ code.py
@@ -4,6 +4,12 @@ df = pd.read_csv("total_stars.csv")
 print(df.columns)
 print(df.shape)
 del df ["Luminosity"]
+del df ["Star_name"]
+del df ["Distance"]
+del df ["Mass"]
+del df ["Radius"]
+del df ["Unnamed: 0"]
+del df ["Unnamed: 6"]
 
 print(df.columns)
 df.to_csv("Final.csv")
